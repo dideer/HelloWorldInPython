c750925afc9b5cee41dcf3e27e5bf1c213a58d71
Ishimwe Didier <didierishimwe221@gmil.com>
2024-01-31 03:00:49 -0800

    first

---
+++ first.py
@@ -0,0 +1,30 @@
+# print("hello world") 
+
+
+
+
+# x,y,z="DIDIER","GWIZA","ISHIMWE"
+
+# print(x)
+# print(y) 
+# print(z) 
+
+
+
+# a=1
+
+# for i in range(1,13,1):
+    
+#     for a in range(1,13,1):
+#             b=i*a
+#          
+#    print(i,'*',a,'=',b)
+        
+#     print()
+
+
+for i in range(1,6,1):
+    for a in range(6,i,-1):
+            print('*',end="")
+    print()
+    
---
+++ work.py
@@ -0,0 +1,158 @@
+# myFirstProgram="Welcome in Year 3 It"
+
+# x="Hello"
+# y="World@"
+# print(x+y)
+
+# fruits=["apple","banana","cherry"]
+# x,y,z=fruits
+
+# print(x)
+# print(y)
+# print(z)
+
+# x="Python"
+# y="Is"
+# z="awesome"
+
+# print(x,y,z)
+# print(x+y+z)
+# x= "Awesome"
+
+# def myFunc():
+#     global x
+#     x= "Fantastic"
+
+#     print("Python Is" , x)
+# myFunc()
+# print("Python Is" , x)
+
+
+# x=5
+# y=10
+
+# print("The Sum of ",x ,"and" ,y ,"is", x+y)
+
+# z=x+y
+
+# print(z)
+
+
+
+# greeting = 'Hello, Lew!'
+# print('1:', greeting)
+# print('2:', len(greeting))
+# print('3:', greeting[0])
+# print('4:', greeting[-1])
+# greeting=greeting.replace("Lew","Class")
+# print('5:', greeting)
+# string1="Hello"
+# string2="world"
+# print("1:",string1,string2)
+# cost=float(35.28)
+# print("Bar tab =€%f" %cost)
+
+
+
+# test_string="Dogs are better than cats"
+# print("Last element:",test_string[4:])
+# print("second to last element:",test_string[:4])
+
+# print(test_string[5:10])
+
+
+
+
+
+# prices= {
+#     "eggs":2.30,
+#     "Steak":13.50,
+#     "Bacon":2.30,
+#     "Beer":14.95,
+# }
+
+# print("1:",prices)
+# print("2:",type(prices))
+# print("The Price of bacon is:",prices["Bacon"])
+
+
+
+
+# name="Ishimwe"
+
+# def myName():
+#     secandName="didier"
+#     print("My Name is",name,secandName)
+# myName()
+
+
+# a="hello, World!"
+# print(a[-1])
+# print()
+
+
+
+
+
+
+
+# for x in "banana":
+    
+#     print(len(x))
+
+
+# for x in range(1,21,1):
+#     if x%2==0:
+#         print(x)
+
+# txt="The best things in life are free!"
+
+# if"free" in txt:
+#     print("yex , 'free' is present.")
+
+# a=3
+# b=5
+
+# if a%2==0 and b%2==0:
+    
+#     print(a+b)
+# else:
+#     print(a*b)
+
+
+
+# a="Welcome in IT Y3"
+
+
+
+# print(a.replace("Y3","LEVEL 7"))
+# print(a.split())
+
+# def myFunction1():
+#     return True
+
+# # print(myFunction)
+
+# def myFunction():
+#     return True
+
+# if myFunction1():
+#     print("yes!")
+# else:
+#     print("No!")
+# x=200
+# print(isinstance(x,int))
+
+# list1 =["apple","banana","cherry"]
+# list2 =[1,5,7,9,3]
+# list3 =[True,False,False]
+
+# thislist= ["apple","banana","cherry","orange","kiwi","mango"]
+# thislist[1:3]= ["blackcurrent","watermelon"]
+
+# print(thislist)
+
+
+number1 = int(input())
+print("Printing type of input value",number1)
+print("type of number ", type(number1))
