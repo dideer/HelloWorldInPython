1db48e9fd75187ce1b716a306dabbd9a2be6afff
Ishimwe Didier <didierishimwe221@gmil.com>
2024-01-31 03:16:55 -0800

    second

--- first.py
+++ first.py
@@ -1,26 +1,25 @@
-# print("hello world") 
+print("hello world") 
 
 
 
 
-# x,y,z="DIDIER","GWIZA","ISHIMWE"
+x,y,z="DIDIER","GWIZA","ISHIMWE"
 
-# print(x)
-# print(y) 
-# print(z) 
+print(x)
+print(y) 
+print(z) 
 
 
 
-# a=1
+a=1
 
-# for i in range(1,13,1):
+for i in range(1,13,1):
     
-#     for a in range(1,13,1):
-#             b=i*a
-#          
-#    print(i,'*',a,'=',b)
+    for a in range(1,13,1):
+            b=i*a
+            print(i,'*',a,'=',b)
         
-#     print()
+print()
 
 
 for i in range(1,6,1):
